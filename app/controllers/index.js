bff172ad5027678f7e9b5cc5759e3080f940a234
AshesVas <ashes.vats@gmail.com>
2016-05-14 16:12:51 +0530

    added front page webhook

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -8,12 +8,12 @@ router.use('/users',require('./users'));
 
 
 // Server frontpage
-app.get('/', function (req, res) {
+router.get('/', function (req, res) {
     res.send('This is TestBot Server');
 });
 
 // Facebook Webhook
-app.get('/webhook', function (req, res) {
+router.get('/webhook', function (req, res) {
     if (req.query['hub.verify_token'] === 'testbot_verify_token') {
         res.send(req.query['hub.challenge']);
     } else {
@@ -21,10 +21,7 @@ app.get('/webhook', function (req, res) {
     }
 });
 
-//home page
-router.get('/',function(req,res) {
-    res.send('This is main controller');
-});
+
 
 
 module.exports=router;
