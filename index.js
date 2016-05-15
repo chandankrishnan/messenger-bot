2a7be601c6f96ad48c4a91da2bcf05d90b74b1e5
AshesVas <ashes.vats@gmail.com>
2016-05-15 12:56:55 +0530

    added

--- index.js
+++ index.js
@@ -28,14 +28,15 @@ app.get('/webhook', function (req, res) {
 });
 
 app.post('/webhook', function (req, res) {
-    var events = req.body.entry[0].messaging;
-    for (i = 0; i < events.length; i++) {
-        var event = events[i];
-        if (event.message && event.message.text) {
-            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
-        }
-    }
-    res.sendStatus(200);
+    console.log(req);
+    // var events = req.body.entry[0].messaging;
+    // for (i = 0; i < events.length; i++) {
+    //     var event = events[i];
+    //     if (event.message && event.message.text) {
+    //         sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+    //     }
+    // }
+    // res.sendStatus(200);
 });
 
 
