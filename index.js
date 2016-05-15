6c0ecb2fff06994a6cac30db0285053a296dc189
AshesVas <ashes.vats@gmail.com>
2016-05-15 13:24:30 +0530

    added message() method

--- index.js
+++ index.js
@@ -43,6 +43,25 @@ app.post('/webhook', function (req, res) {
     res.sendStatus(200);
 });
 
+// generic function sending messages
+function sendMessage(recipientId, message) {
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        method: 'POST',
+        json: {
+            recipient: {id: recipientId},
+            message: message,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+};
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
