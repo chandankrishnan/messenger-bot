b35b528aa20117e08dba03f34fc7b304ffd5a808
AshesVas <ashes.vats@gmail.com>
2016-05-15 13:18:31 +0530

    added request and bodyParser

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var request = require('request');
 
-app.set('port', (process.env.PORT || 5000));
-
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
