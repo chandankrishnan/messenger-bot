6f785da0b795b3405eb437d10b5af1abc0cdd044
AshesVas <ashes.vats@gmail.com>
2016-05-19 15:15:21 +0530

    fixed else if

--- controllers/fbctrl.js
+++ controllers/fbctrl.js
@@ -61,8 +61,7 @@ function isPostback(event)
 function isMessage(event)
 {
     if( event.message && event.message.text){
-        console.log('this is message request');
-        console.log(event);
+console.log('message detected');
         return true;
     }
     return false;
@@ -104,16 +103,16 @@ router.post('/webhook', function (req, res)
 {
 
     var events = req.body.entry[0].messaging;
-    console.log('event recived ' + event);
+
     for (i = 0; i < events.length; i++)
     {
         var event = events[i];
-        console.log(event);
+        console.log('event recived ' + event);
         if(isPostback(event))
         {
             postback(event,event.sender.id);
         }
-        else if(isMessage())
+        else if(isMessage(event))
         {
             message(event,event.sender.id);
         }
