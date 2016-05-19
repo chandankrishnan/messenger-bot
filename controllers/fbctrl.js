90a42dff8ad47ef06e65c07736c23e777a463f7f
AshesVas <ashes.vats@gmail.com>
2016-05-19 15:48:59 +0530

    added delivery report

--- controllers/fbctrl.js
+++ controllers/fbctrl.js
@@ -1,77 +1,71 @@
-var express=require('express')
-    ,router=express.Router(),
+var express = require('express')
+    , router = express.Router(),
     request = require('request'),
-    template=require('./../helpers/template'),
+    template = require('./../helpers/template'),
     graph = require('fbgraph'),
-    token=process.env.PAGE_ACCESS_TOKEN.toString(),
-    userList=require('./../models/users'),
-    user=new userList();
+    token = process.env.PAGE_ACCESS_TOKEN.toString(),
+    userList = require('./../models/users'),
+    user = new userList();
 
 
 graph.setAccessToken(token);
 
-function postback(data,sender_id)
-{
+// handle postback request form messenger
+function postback(data, sender_id) {
 
-    var response=data.postback.payload.toString().trim().toLowerCase();
+    var response = data.postback.payload.toString().trim().toLowerCase();
     console.log('inside postback ' + response);
-  switch(response)
-  {
-    case 'accept-friend-request':
-          console.log('accept-friend-request payload fired ');
-          sendMessage(sender_id,'Thanks for registering with us.');
-          break;
-    case 'decline-friend-request':
-        console.log('decline friend request');
-          sendMessage(sender_id,'See you soon');
-          break;
-  }
+    switch (response) {
+        case 'accept-friend-request':
+            console.log('accept-friend-request payload fired ');
+            sendMessage(sender_id, 'Thanks for registering with us.');
+            break;
+        case 'decline-friend-request':
+            console.log('decline friend request');
+            sendMessage(sender_id, 'See you soon');
+            break;
+    }
 }
 
-function message(data,sender_id)
-{
-    var text=data.message.text.toLowerCase();
+//handle message received event.
+function messageReceive(data, sender_id) {
+    var text = data.message.text.toLowerCase();
 
-    switch(text)
-    {
+    switch (text) {
         case 'hi':
-            sendMessage(sender_id,template.welcome());
+            sendMessage(sender_id, template.welcome());
             break;
         case 'testpostback':
-            sendMessage(sender_id,'your answer recived');
+            sendMessage(sender_id, 'your answer recived');
             break;
 
     }
 }
 
-function defaultMessage(_id)
-{
-  sendMessage(_id, template.defaultMessage());
+//handle delivery report event.
+function deliveryReport(event, sender_id) {
+    console.log('delivery report recived');
+    console.log(event);
 }
 
-function isPostback(event)
-{
-//skip user registration check on accepting and rejecting friend request
-    if(event.postback && event.postback.payload != ""){
-        console.log('this is postback request');
-       return true;
-    }
-    return false;
+
+//check weather request is postback request or not
+function isPostback(event) {
+    return event.postback && event.postback.payload != "";
+}
+//check weather request is message recived or not
+function isMessageReceive(event) {
+    return event.message && event.message.text;
+}
+//check weather request is  delivery report
+function isDeliveryReport() {
+    return event.delivery ? true : false;
 }
 
-function isMessage(event)
-{
-    if( event.message && event.message.text){
-console.log('message detected');
-        return true;
-    }
-    return false;
-
-}
-// generic function sending messages
+// generic function to send message
 function sendMessage(recipientId, text) {
-  messageData = {
-      text: text
+    messageData = {
+        text: text
     }
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
@@ -81,7 +75,7 @@ function sendMessage(recipientId, text) {
             recipient: {id: recipientId},
             message: messageData.text,
         }
-    }, function(error, response, body) {
+    }, function (error, response, body) {
         if (error) {
             console.log('Error sending message: ', error);
         } else if (response.body.error) {
@@ -92,7 +86,7 @@ function sendMessage(recipientId, text) {
 
 // Facebook Webhook
 router.get('/webhook', function (req, res) {
-  console.warn('authentication called');
+    console.warn('authentication called');
     if (req.query['hub.verify_token'] === 'testbot_verify_token') {
         res.send(req.query['hub.challenge']);
     } else {
@@ -100,42 +94,42 @@ router.get('/webhook', function (req, res) {
     }
 });
 
-router.post('/webhook', function (req, res)
-{
+
+// middleware to receive fb hooks
+router.post('/webhook', function (req, res) {
 
     var events = req.body.entry[0].messaging;
 
-    for (i = 0; i < events.length; i++)
-    {
+    for (i = 0; i < events.length; i++) {
         var event = events[i];
-        console.log( event);
-        if(isPostback(event))
-        {
-            postback(event,event.sender.id);
+        console.log(event);
+        if (isDeliveryReport(event)) {
+            deliveryReport(event, event.sender.id);
         }
-        else if(isMessage(event))
-        {
-            message(event,event.sender.id);
+        else if (isPostback(event)) {
+            postback(event, event.sender.id);
+        }
+        else if (isMessageReceive(event)) {
+            messageReceive(event, event.sender.id);
         }
     }
     res.sendStatus(200);
 });
 
 
-
 //user exist in database
 user.once('user-exist', function () {
     console.log('user exist fired');
-      sendMessage(_id, template.welcome());
+    sendMessage(_id, template.welcome());
 })
 
 //user not exist in database
 user.once('user-not-exist', function (_id) {
-  console.log('user not found event fired');
+    console.log('user not found event fired');
 
-  graph.get(_id, function(err, res){
-      sendMessage(_id, template.welcome(res.first_name + ' ' +res.last_name));
-  });
+    graph.get(_id, function (err, res) {
+        sendMessage(_id, template.welcome(res.first_name + ' ' + res.last_name));
+    });
 });
 
-module.exports=router;
+module.exports = router;
