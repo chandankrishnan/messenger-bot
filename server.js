be7d2a75530e138eae2bbc850c503792e9360bf2
AshesVas <ashes.vats@gmail.com>
2016-05-31 17:43:46 +0530

    init

--- server.js
+++ server.js
@@ -14,28 +14,8 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
 //middelware to load controllers
-//app.use(require('./controllers'))
+app.use(require('./controllers'))
 
-router.get('/fb/webhook', function (req, res) {
-    console.warn('authentication called');
-    if (req.query['hub.verify_token'] === 'testbot_verify_token') {
-        res.send(req.query['hub.challenge']);
-    } else {
-        res.send('Invalid verify token');
-    }
-});
-
-
-router.post('/fb/webhook',function(req,res,next){
-   console.log('recived request');
-    res.sendStatus(200);
-});
-
-router.get('/',function(req,res){
-    res.send('c=main');
-});
-console.log("app staring");
-app.use('/',router);
 app.set('port', PORT);
 
 http.createServer(app).listen(PORT);
