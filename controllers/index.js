39777689b4d9845e88380f6eeacbfb1742ac6ac0
AshesVas <ashes.vats@gmail.com>
2016-06-09 11:41:05 +0530

    fixed url

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@ var express=require('express')
     ,router=express.Router()
 
 
-router.use('/fb/v1',require('./fbCtrl'));
+router.use('/fb',require('./fbCtrl'));
 router.use('/',function(req,res,next){
   next();
 })
