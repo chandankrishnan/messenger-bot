c4f420e3e5f901ca29564c24e87cd8af33d06351
ashes vats <ashes.vats@gmail.com>
2016-08-29 23:32:07 -0700

    fixed 2

--- controllers/fbCtrl.js
+++ controllers/fbCtrl.js
@@ -25,21 +25,20 @@ const firstEntityValue = function (entities, entity) {
     else return false;
 
 }
-//TODO:: delete getFirstMessageEntry function
-// const getFirstMessagingEntry = (body) => {
-//     const val = body.object == 'page' &&
-//             body.entry &&
-//             Array.isArray(body.entry) &&
-//             body.entry.length > 0 &&
-//             body.entry[0] &&
-//             body.entry[0].id == FB_PAGE_ID &&
-//             body.entry[0].messaging &&
-//             Array.isArray(body.entry[0].messaging) &&
-//             body.entry[0].messaging.length > 0 &&
-//             body.entry[0].messaging[0]
-//         ;
-//     return val || null;
-// };
+const getFirstMessagingEntry = (body) => {
+    const val = body.object == 'page' &&
+            body.entry &&
+            Array.isArray(body.entry) &&
+            body.entry.length > 0 &&
+            body.entry[0] &&
+            body.entry[0].id == FB_PAGE_ID &&
+            body.entry[0].messaging &&
+            Array.isArray(body.entry[0].messaging) &&
+            body.entry[0].messaging.length > 0 &&
+            body.entry[0].messaging[0]
+        ;
+    return val || null;
+};
 
 // This will contain all user sessions.
 // Each session has an entry:
